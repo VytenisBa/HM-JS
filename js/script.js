a1abf40037b94492514f72c2b52bfe5445f4e202
VytenisBa <vbgithub@gmail.com>
2021-11-29 21:07:30 +0200

    9 uzduotys

---
+++ js/script.js
@@ -0,0 +1,174 @@
+
+console.log("Hello");
+
+//---------1-------
+/*
+let n;
+n = 0;
+if(n > 0){
+    console.log(n + ' are positive number');
+}    
+else if (n == 0) {
+    console.log (n + ' zero are zero:)')
+}
+else console.log(n + ' are negative number');
+*/
+
+//--------2------
+/*
+let n;
+n = 2;
+
+if(n == 1){
+    console.log('GREEN you may go');
+}
+else if (n == 2){
+    console.log('WAIT!!! its yellow light!');
+}
+else if (n == 3){
+    console.log('RED!!! STOP');
+}
+else console.log('ERROR! Traffic light out of order');
+*/
+
+//--------3------
+/*
+let d;  //dezes     2
+let k;  //knygos    8   
+let n;  //talpa     5
+
+d = 2;
+k = 100;
+n = 5;
+
+if(d * n / k >= 1){
+    console.log('Books fit to boxes');
+}
+else if (d * n / k >= 0){
+    console.log('Books doesnt fit to boxes');
+}        
+*/
+
+//-------4------
+/*
+let n;
+n = 1221;
+
+let a = Math.floor( n % 10);
+let b = Math.floor (n / 10 % 10);
+let c = Math.floor (n / 100 % 10);
+let d = Math.floor (n / 1000 % 10);
+
+if(a == d){
+    if(b == c){
+        console.log('Yes its plindrome');
+    }
+    else {console.log('No this is not pilindrome');
+    }
+}
+else console.log('No this is not pilindrome');
+*/
+
+//-------5-------
+/*
+let n;
+n = 222442;
+
+let a = Math.floor (n % 10);
+let b = Math.floor (n / 10 % 10);
+let c = Math.floor (n / 100 % 10);
+let d = Math.floor (n / 1000 % 10);
+let e = Math.floor (n / 10000 % 10);
+let f = Math.floor (n / 100000 % 10);
+
+if(a == d){
+    if((a+b+c+d+e+f) / 4 == 4){
+        console.log('Lucky');
+    }
+    else {console.log('No luck this time');
+    }
+}
+else console.log('No luck this time');
+*/
+
+//--------6------
+/*
+let year;
+year = 2004;
+
+if (0 == year % 4) {
+    console.log(year + ' is a leap year');
+} else {
+    console.log(year + ' is not a leap year');
+}
+*/
+
+//--------7-----
+
+/*
+let n;
+n = 12423;
+
+let a = Math.floor (n % 10);
+let b = Math.floor (n / 10 % 10);
+let c = Math.floor (n / 100 % 10);
+let d = Math.floor (n / 1000 % 10);
+let e = Math.floor (n / 10000 % 10);
+
+if(a == e){
+    if(d == b){
+        console.log('Yes its plindrome');
+    }
+    else {console.log('No this is not pilindrome');
+    }
+}
+else console.log('No this is not pilindrome');
+
+*/
+
+//----------8----------
+
+/*
+let k;
+k = 8;
+
+if(k == 1){
+    console.log('Room cleaning junior');
+}
+else if (k == 3){
+    console.log('Room cleaning junior');
+}
+else if (k == 5){
+    console.log('Room cleaning junior');
+}
+else if (k == 4){
+    console.log('Room cleaning older one');
+}
+else if (k == 6){
+    console.log('Room cleaning older one');
+}
+else if (k > 6){
+    console.log('No cheating- roll the dice again!');
+} 
+*/
+
+//---------9--------
+/*
+
+let p1 = 5, p2 = 8, p3 = 9;
+
+if(p1 == 9){
+    console.log('p1 are best student of the month');
+}
+else if(p2 == 9){
+    console.log('p2 are best student of the month');
+}
+else if(p3 == 9){
+    console.log('p3 are best student of the month');
+}
+*/
+
+//----------10-------
+
+
+
